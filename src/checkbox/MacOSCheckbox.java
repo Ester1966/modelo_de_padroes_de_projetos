28c895ef0d237a9bb3f4c4c1500fcdeb1a93be88
Ester1966 <127158870+Ester1966@users.noreply.github.com>
2023-03-20 22:18:31 -0300

    Update MacOSCheckbox.java

--- src/checkbox/MacOSCheckbox.java
+++ src/checkbox/MacOSCheckbox.java
@@ -6,3 +6,27 @@ public class MacOSCheckbox implements Checkbox{
     	System.out.println("You have created MacOSCheckbox.");
     }
 }
+
+
+//O código apresentado define uma classe chamada "MacOSCheckbox" que implementa a interface "Checkbox".
+
+A classe "MacOSCheckbox" sobrescreve o método "paint()" definido na interface "Checkbox". Dentro do método, há uma instrução que imprime uma mensagem
+indicando que um checkbox do macOS foi criado.
+
+Essa classe representa uma implementação concreta de um checkbox para o sistema operacional macOS. Ao chamar o método "paint()" em uma instância de 
+"MacOSCheckbox", a mensagem "You have created MacOSCheckbox." será exibida na saída padrão do sistema.
+
+Essa implementação do método "paint()" é específica para o checkbox do macOS, ou seja, a mensagem impressa no console é referente à criação de um
+checkbox do macOS.
+
+Ao implementar a interface "Checkbox", a classe "MacOSCheckbox" se compromete a implementar todos os métodos abstratos da interface. Isso é importante
+porque permite que diferentes implementações de checkboxes possam ser criadas e usadas em diferentes partes do código, sem que as implementações específicas 
+precisem ser conhecidas por essas partes do código.
+
+Em resumo, a classe "MacOSCheckbox" é uma parte importante de um sistema de classes que implementa diferentes tipos de checkboxes para uma interface de usuário, 
+pois fornece uma implementação específica para um checkbox no sistema operacional macOS.//
+
+
+
+
+
