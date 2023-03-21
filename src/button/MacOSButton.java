04f2a65e4e0ce734ef073b7623abbb063d27a790
Ester1966 <127158870+Ester1966@users.noreply.github.com>
2023-03-20 22:11:07 -0300

    Update MacOSButton.java

--- src/button/MacOSButton.java
+++ src/button/MacOSButton.java
@@ -7,3 +7,25 @@ public class MacOSButton implements Button{
     	
     }
 }
+// O código apresentado define uma classe chamada "MacOSButton" que implementa a interface "Button".
+
+A classe "MacOSButton" sobrescreve o método "paint()" definido na interface "Button". Dentro do método, há uma instrução 
+que imprime uma mensagem indicando que um botão do MacOS foi criado.
+
+Essa classe representa uma implementação concreta de um botão para o sistema operacional MacOS. Ao chamar o método "paint()" em uma instância
+de "MacOSButton", a mensagem "You have created MacOSButton." será exibida na saída padrão do sistema.
+
+Essa implementação do método "paint()" é específica para o botão do MacOS, ou seja, a mensagem impressa no console é referente à criação de um botão do MacOS.
+
+Ao implementar a interface "Button", a classe "MacOSButton" se compromete a implementar todos os métodos abstratos da interface. Isso é importante porque permite
+
+que diferentes implementações de botões possam ser criadas e usadas em diferentes partes do código, sem que as implementações específicas precisem ser conhecidas
+por essas partes do código.
+
+Em resumo, a classe "MacOSButton" é uma parte importante de um sistema de classes que implementa diferentes tipos de botões para uma interface de usuário,
+pois fornece uma implementação específica para um botão no sistema operacional MacOS.//
+
+
+
+
+
