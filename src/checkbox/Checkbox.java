446c0bf4a2679a046a6f38dec6f420604b87f1e5
Ester1966 <127158870+Ester1966@users.noreply.github.com>
2023-03-20 22:15:26 -0300

    Update Checkbox.java

--- src/checkbox/Checkbox.java
+++ src/checkbox/Checkbox.java
@@ -4,3 +4,10 @@ public class Checkbox {
 	void paint();
 
 }
+
+O código apresentado define uma classe chamada "Checkbox".
+
+Dentro da classe, há a declaração de um método "paint()" sem implementação.
+
+A falta da palavra reservada "abstract" indica que o método "paint()" não é um método abstrato, ou seja, ele deve ter 
+uma implementação em classes concretas que implementam essa classe.//
