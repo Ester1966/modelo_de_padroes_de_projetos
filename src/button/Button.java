7696e4f0085e6f2843c843c3e1ef75317ca3615c
Ester1966 <127158870+Ester1966@users.noreply.github.com>
2023-03-20 22:08:31 -0300

    Update Button.java

--- src/button/Button.java
+++ src/button/Button.java
@@ -3,3 +3,11 @@ package button;
 public interface Button {
     void paint();
 }
+
+
+//Uma interface é um tipo de referência em Java, que define um conjunto de métodos abstratos e constantes que podem ser implementados por classes. 
+No caso desta interface, ela possui apenas um método abstrato chamado "paint()" que não possui implementação e não retorna nenhum valor.
+
+O objetivo dessa interface é definir um contrato que qualquer classe que implemente essa interface deve seguir. Ou seja, qualquer classe que 
+implemente a interface "Button" deve obrigatoriamente implementar o método "paint()". A implementação desse método pode variar de acordo com a
+classe que a implementa, mas a assinatura do método deve ser a mesma.//
