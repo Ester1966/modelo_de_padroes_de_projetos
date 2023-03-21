3ef641a6ca2fb453eb075a2c4236f2b380dfbbb8
Ester1966 <127158870+Ester1966@users.noreply.github.com>
2023-03-20 22:00:38 -0300

    Update Application.java

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,5 +1,5 @@
 package app;
-
+// 
 import button.Button;
 import factories.GUIFactory;
 
@@ -11,10 +11,17 @@ public class Application {
 		button = factory.createButton();
 		checkbox = factory.createCheckbox();
 	}
-	
+		
 	public void paint() {
 		button.paint();
 		checkbox.paint();
 	}
 
 }
+//Este código representa a classe "Application" que é responsável por criar uma interface de usuário com um botão e uma caixa de seleção (checkbox). 
+A interface de usuário é criada através de um factory  GUI (GUIFactory) que é fornecida ao construtor da classe.
+
+A primeira coisa que é feita na classe é importar as classes Button e GUIFactory, que são utilizadas posteriormente. Em seguida, a classe declara duas variáveis
+privadas: button e checkbox.
+
+O construtor da classe recebe como parâmetro uma instância de GUIFactory que será utilizada para criar os componentes da interface de usuário. O método "createButton()" é chamado na fábrica e o resultado é armazenado na variável "button". O mesmo é feito para a criação da caixa de seleção (checkbox), que é armazenada na variável "checkbox".
