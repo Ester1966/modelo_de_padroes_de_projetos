45f01d52bf2dd33783f3b29b6c72a245c5251197
Ester1966 <127158870+Ester1966@users.noreply.github.com>
2023-05-15 20:41:57 -0300

    Add files via upload

---
+++ projetoSingleton/src/projetosingleton/Janela.java
@@ -0,0 +1,51 @@
+package projetosingleton;
+
+/**
+ *
+ * @author Ester Leonardo
+ */
+public class Janela {
+
+ private static final Janela INSTANCE = new Janela();
+
+ private static final String Teste= "Exemplo Singleton Privado";
+
+
+private static final String Singleton = "Exemplo Singleton Publico";
+
+private Janela() {
+
+ }
+
+
+public static Janela getInstance() {
+
+    return INSTANCE;
+
+ }
+
+public static void Abir() {
+
+System.out.println("Abrir Janela!");
+
+ }
+
+public static void Fechar() {
+
+System.out.println("Abrir Fechar");
+
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ projetoSingleton/src/projetosingleton/Janela2.java
@@ -0,0 +1,35 @@
+package projetosingleton;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class Janela2 { 
+
+
+
+private static Janela2 instancia;
+
+private Janela2() {
+
+
+}
+
+
+ public static synchronized Janela2 getInstance() {
+
+if(instancia == null) {
+
+instancia = new Janela2();
+
+}
+
+return instancia;
+
+}
+
+
+}
+
+    
+
